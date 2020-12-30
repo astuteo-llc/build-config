e804e2e14431878a5c526cef41668496b9e649c9
Dustin Walker <me@dustinwalker.com>
2020-12-30 11:17:49 -0600

    ADD: Example content

---
+++ example.webpack.mix.js
@@ -0,0 +1,131 @@
+/**
+ * CORE ASTUTEO BUILD SYSTEM
+ * Version 5.0 | Updated: 12/2020
+ *
+ * Astuteo Laravel Mix Config
+ * Requires Laravel Mix 6.x
+ *
+ * 1. Include external dependencies
+ * 2. Generate project banner
+ * 3. Laravel Mix Config
+ *    a. Javascript config and files as defined in config/mix/project-config
+ *    b. Include Sass files as defined in config/mix/project-config
+ *    c. Copy image files as defined in config/mix/project-config
+ *    d. Copy font files as defined in config/mix/project-config
+ *    e. Copy any static directories as defined in config/mix/project-config
+ *    f. Browsersync settings as defined in config/mix/project-config
+ */
+
+/**
+ * 1. INCLUDE EXTERNAL DEPENDENCIES
+ */
+let project = require('./config/mix/project-config');
+let mix = require('laravel-mix');
+let pkg = require('./package.json');
+
+require('laravel-mix-polyfill');
+require('laravel-mix-banner');
+
+let domain = project.defaultDomain;
+try {
+    domain = require('./config/mix/local-config').domain
+} catch (e) {
+    console.log('local-config.js not found, using default ' + domain);
+}
+
+/**
+ * 2. GENERATE PROJECT BANNER
+ */
+Date.prototype.mmddyyyy = function () {
+    let mm = this.getMonth() + 1; // getMonth() is zero-based
+    let dd = this.getDate();
+    return [
+        (mm > 9 ? "" : "0") + mm,
+        (dd > 9 ? "" : "0") + dd,
+        this.getFullYear(),
+    ].join("");
+};
+let date = new Date();
+date = date.mmddyyyy();
+
+if(typeof(pkg.project) === 'undefined') {
+    console.warn('[WARNING] missing project key in package.json');
+}
+if(typeof(pkg.author) === 'undefined') {
+    console.warn('[WARNING] missing author key in package.json');
+}
+if(typeof(pkg.version) === 'undefined') {
+    console.warn('[WARNING] missing version key in package.json');
+}
+const banner = [
+    '/**',
+    ' * @project        ' + pkg.project,
+    ' * @author         ' + pkg.author,
+    ' * @date           ' + date,
+    ' * @release        ' + pkg.version,
+    ' */',
+    '',
+]
+
+/**
+ * 3. LARAVEL MIX CONFIG
+ */
+mix.setPublicPath(project.dir.public);
+/**
+ * 3A. JAVASCRIPT
+ */
+mix.js(project.jsFiles, project.dir.js)
+    .polyfill({
+        enabled: true,
+        debug: false,
+        useBuiltIns: "usage",
+        targets: false // setting to false will cause it to read from .browserslistrc
+    }).sourceMaps()
+    .banner({
+        banner: (function () {
+            return banner.join('\n');
+        })(),
+        raw: true,
+    })
+    .version();
+
+/**
+ * 3B. SASS
+ */
+project.sassFiles.forEach(file => {
+    mix.sass(file, 'css', {})
+        .options({
+            postCss: [require('tailwindcss')(project.tailwindconfig)],
+        }).version();
+})
+/**
+ * 3C. IMAGES
+ */
+project.imageDirectories.forEach(directory => {
+    mix.copy(directory + '/**/*', project.dir.public + '/' + project.dir.images);
+});
+/**
+ * 3D. FONTS
+ */
+if(typeof(project.fontDirectories) != 'undefined' && project.fontDirectories.length > 0 ) {
+    project.fontDirectories.forEach(directory => {
+        mix.copy(directory + '/**/*', project.dir.public + '/' + project.dir.fonts);
+    });
+}
+/**
+ * 3E. STATIC DIRECTORIES
+ */
+if(typeof(project.staticDirectories) != 'undefined' && project.staticDirectories.length > 0 ) {
+    project.staticDirectories.forEach(directory => {
+        mix.copy(directory.src + '/**/*', directory.dest);
+    });
+}
+/**
+ * 3F. STATIC DIRECTORIES
+ */
+mix.browserSync(
+    {
+        proxy: domain,
+        files: project.watchFiles
+    }
+);
