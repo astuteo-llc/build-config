dec0cedb3a42b69d6d2cff152b26656cc8ad6f48
Dustin Walker <me@dustinwalker.com>
2021-02-09 16:26:46 -0600

    ADD: Example src ref

---
+++ reference/src/js/app.js
@@ -0,0 +1,4 @@
+console.log('app js');
+
+import('./parts');
+import('./modules');
---
+++ reference/src/js/modules/index.js
@@ -0,0 +1,13 @@
+/*
+  Automatically instantiates modules based on data-attributes
+  specifying module file-names.
+*/
+
+const moduleElements = document.querySelectorAll("[data-module]");
+
+for (let i = 0; i < moduleElements.length; i++) {
+    const el = moduleElements[i];
+    const name = el.getAttribute("data-module");
+    const Module = require(`./${name}`).default;
+    new Module(el);
+}
